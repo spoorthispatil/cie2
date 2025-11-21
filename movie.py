5af1eb5934a17218db428cb05d0c36a7315b8cd0
spoorthispatil <spoorthipatil219@gmail.com>
2025-11-21 09:46:07 +0530

    save

---
+++ movie.py
@@ -0,0 +1,20 @@
+import sys
+if len(sys.argv)==6:
+    script_name=sys.argv[0]
+    name_=sys.argv[1]
+    duration=float(sys.argv[2])
+    language=sys.argv[3]
+    price=float(sys.argv[4])
+    print("user provided information.")
+else:
+    script_name=sys.argv[0]
+    name="RRR"
+    duration=3.5
+    language="Telugu"  
+    price=250.0
+    print("No input given - using default information.")
+print("Script Name:",script_name)
+print("Movie Name:",name_)
+print("Duration:{duration} hours")
+print("Language:",language)
+print("Price: Rs.",price)
